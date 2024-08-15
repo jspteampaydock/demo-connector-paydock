9607d88a2bbe08d3cfa934d7daecbf1878a82ae7
paydock.jsp.development <paydock.jsp.development@jetsoftpro.com>
2024-08-15 15:45:56 +0300

    Fixed problem with connect.yml

--- extension/src/config/config-loader.js
+++ extension/src/config/config-loader.js
@@ -1,22 +1,40 @@
-// import rc from 'rc'
-import {config} from "dotenv";
+import { config } from "dotenv";
+
+const requiredEnvVars = [
+    'COMMERCETOOLS_CLIENT_ID',
+    'COMMERCETOOLS_CLIENT_SECRET',
+    'COMMERCETOOLS_PROJECT_KEY',
+    'COMMERCETOOLS_API_URL',
+    'COMMERCETOOLS_AUTH_URL',
+    'PAYDOCK_API_LIVE_URL',
+    'PAYDOCK_API_LIVE_URL'
+];
 
 function loadConfig() {
-  config();
-  if (process.env.PAYDOCK_INTEGRATION_CONFIG) {
-    return loadFromPaydockIntegrationEnvVar()
-  }
-  return {}
+    config();
+    if (requiredEnvVars.every(varName => process.env[varName])) {
+        return loadFromPaydockIntegrationEnvVar();
+    }
+    return {};
 }
 
 function loadFromPaydockIntegrationEnvVar() {
-  try {
-    return JSON.parse(process.env.PAYDOCK_INTEGRATION_CONFIG)
-  } catch (e) {
-    throw new Error(
-        `Paydock integration configuration is not provided in the JSON format`,
-    )
-  }
+    const envConfig = {
+        clientId: process.env.COMMERCETOOLS_CLIENT_ID,
+        clientSecret: process.env.COMMERCETOOLS_CLIENT_SECRET,
+        projectKey: process.env.COMMERCETOOLS_PROJECT_KEY,
+        apiUrl: process.env.COMMERCETOOLS_API_URL,
+        authUrl: process.env.COMMERCETOOLS_AUTH_URL,
+        paydockSandboxUrl: process.env.PAYDOCK_API_SANDBOX_URL,
+        paydockLiveUrl: process.env.PAYDOCK_API_LIVE_URL,
+        extensionBaseUrl: process.env.EXTENSION_BASE_URL
+    };
+    const missingVars = requiredEnvVars.filter(varName => !process.env[varName]);
+    if (missingVars.length > 0) {
+        throw new Error(`Missing required environment variables: ${missingVars.join(', ')}`);
+    }
+
+    return envConfig;
 }
 
-export { loadConfig }
+export { loadConfig };
--- notification/src/config/config-loader.js
+++ notification/src/config/config-loader.js
@@ -1,20 +1,40 @@
-import {config} from "dotenv";
+import { config } from "dotenv";
+
+const requiredEnvVars = [
+  'COMMERCETOOLS_CLIENT_ID',
+  'COMMERCETOOLS_CLIENT_SECRET',
+  'COMMERCETOOLS_PROJECT_KEY',
+  'COMMERCETOOLS_API_URL',
+  'COMMERCETOOLS_AUTH_URL',
+  'PAYDOCK_API_LIVE_URL',
+  'PAYDOCK_API_LIVE_URL'
+];
 
 function loadConfig() {
   config();
-  if (process.env.PAYDOCK_INTEGRATION_CONFIG) {
-    return loadFromPaydockIntegrationEnvVar()
+  if (requiredEnvVars.every(varName => process.env[varName])) {
+    return loadFromPaydockIntegrationEnvVar();
   }
-  return {}
+  return {};
 }
 
 function loadFromPaydockIntegrationEnvVar() {
-  try {
-    return JSON.parse(process.env.PAYDOCK_INTEGRATION_CONFIG)
-  } catch (e) {
-    throw new Error(
-        `Paydock integration configuration is not provided in the JSON format`,
-    )
+  const envConfig = {
+    clientId: process.env.COMMERCETOOLS_CLIENT_ID,
+    clientSecret: process.env.COMMERCETOOLS_CLIENT_SECRET,
+    projectKey: process.env.COMMERCETOOLS_PROJECT_KEY,
+    apiUrl: process.env.COMMERCETOOLS_API_URL,
+    authUrl: process.env.COMMERCETOOLS_AUTH_URL,
+    paydockSandboxUrl: process.env.PAYDOCK_API_SANDBOX_URL,
+    paydockLiveUrl: process.env.PAYDOCK_API_LIVE_URL,
+    notificationBaseUrl: process.env.NOTIFICATION_BASE_URL
+  };
+  const missingVars = requiredEnvVars.filter(varName => !process.env[varName]);
+  if (missingVars.length > 0) {
+    throw new Error(`Missing required environment variables: ${missingVars.join(', ')}`);
   }
+
+  return envConfig;
 }
-export { loadConfig }
+
+export { loadConfig };
