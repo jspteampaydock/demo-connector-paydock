fc5623549284b67b243b072e8ce6283ec939aaf2
jspteampaydock <157017480+jspteampaydock@users.noreply.github.com>
2024-08-21 15:53:03 +0300

    Fixed problem with apm

--- extension/src/service/web-component-service.js
+++ extension/src/service/web-component-service.js
@@ -55,7 +55,7 @@ async function makePayment(makePaymentRequestObj) {
         customerId = await getCustomerIdByVaultToken(input.CommerceToolsUserId, vaultToken);
     }
 
-    response = await handlePaymentType(input, configurations, vaultToken, customerId, amount, currency, paymentType);
+    response = await handlePaymentType(input, configurations, vaultToken, customerId, amount, currency, paymentType, paymentSource);
 
     if (response) {
         status = response.status;
@@ -75,7 +75,7 @@ async function makePayment(makePaymentRequestObj) {
     return response;
 }
 
-async function handlePaymentType(input, configurations, vaultToken, customerId, amount, currency, paymentType) {
+async function handlePaymentType(input, configurations, vaultToken, customerId, amount, currency, paymentType, paymentSource) {
 
     try {
         switch (paymentType) {
@@ -99,6 +99,7 @@ async function handlePaymentType(input, configurations, vaultToken, customerId, 
                     input,
                     amount,
                     currency,
+                    paymentSource,
                     paymentType
                 });
             case 'PayPal Smart':
