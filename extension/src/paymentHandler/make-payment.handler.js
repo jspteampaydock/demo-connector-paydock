94b36756a82dc68bda8b94578eec3f14e6d1f550
jspteampaydock <157017480+jspteampaydock@users.noreply.github.com>
2024-09-18 11:24:59 +0300

    FIxed problem with additional info

--- extension/src/paymentHandler/make-payment.handler.js
+++ extension/src/paymentHandler/make-payment.handler.js
@@ -7,9 +7,9 @@ import c from '../config/constants.js'
 import {makePayment} from '../service/web-component-service.js'
 
 async function execute(paymentObject) {
-
     const paymentExtensionRequest = JSON.parse(paymentObject.custom.fields.PaymentExtensionRequest)
     const makePaymentRequestObj = paymentExtensionRequest?.request
+    const additionalInfo  = makePaymentRequestObj.AdditionalInfo;
     let capturedAmount = paymentObject.amountPlanned.centAmount;
     if (paymentObject.amountPlanned.type === 'centPrecision') {
         const fraction = 10 ** paymentObject.amountPlanned.fractionDigits;
@@ -29,8 +29,8 @@ async function execute(paymentObject) {
         };
     }
     const paydockStatus = response?.paydockStatus ?? makePaymentRequestObj?.PaydockPaymentStatus;
+    makePaymentRequestObj.AdditionalInfo = additionalInfo;
     actions = generateActionsFromResponse(actions, response, makePaymentRequestObj, capturedAmount, paymentObject, paydockStatus);
-
     if (paydockStatus) {
         const {orderState, orderPaymentState} = getCommercetoolsStatusesByPaydockStatus(paydockStatus);
         actions.push(createSetCustomFieldAction(c.CTP_INTERACTION_PAYMENT_EXTENSION_RESPONSE, JSON.stringify({
@@ -74,10 +74,8 @@ function generateActionsFromResponse(actions, response, requestBodyJson, capture
     const makePaymentRequestObj = paymentExtensionRequest?.request
     const updatePaymentAction = getPaymentKeyUpdateAction(paymentObject.key, makePaymentRequestObj, response);
     if (updatePaymentAction) actions.push(updatePaymentAction);
-
     const addTransactionAction = createAddTransactionActionByResponse(paymentObject.amountPlanned.centAmount, paymentObject.amountPlanned.currencyCode, response);
     if (addTransactionAction) actions.push(addTransactionAction);
-
     return actions;
 }
 
